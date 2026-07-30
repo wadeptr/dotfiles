d4f40684ae76cfcd33bf7b46b386758f9d2b5ec8
Brock Wade <brock@brockwade.com>
2026-07-29 20:22:19 -0700

    configure home development tmux session

--- .config/tmux/global-dev-session.sh
+++ .config/tmux/global-dev-session.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-SESSION="cortex-dev"
+SESSION="home-dev"
 
-tmux has-session -t $SESSION 2>/dev/null
-if [ $? -eq 0 ]; then
-  exec tmux attach -t $SESSION
+if tmux has-session -t "=$SESSION" 2>/dev/null; then
+  exec tmux attach-session -t "$SESSION"
 fi
 
-tmux new-session -d -s $SESSION -n "local-dev"
-tmux new-window -t $SESSION -n "local-server"
+tmux new-session -d -s "$SESSION" -n "local-dev"
+tmux new-window -t "$SESSION" -n "training"
+tmux new-window -t "$SESSION" -n "agent"
+tmux new-window -t "$SESSION" -n "local-server"
 
-tmux select-window -t $SESSION:local-dev
-exec tmux attach -t $SESSION
-
+tmux select-window -t "$SESSION:local-dev"
+exec tmux attach-session -t "$SESSION"
